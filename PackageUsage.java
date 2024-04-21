406daeea74634e4050f3244c3d0c1b9aef79b6af
MS ALOK KUMAR SAHOO <149507288+alokcorder92@users.noreply.github.com>
2024-04-21 07:27:21 +0530

    Add files via upload

---
+++ PackageUsage.java
@@ -0,0 +1,8 @@
+import mypack.PackageExample;
+
+public class PackageUsage{
+    public static void main(String[] ar){
+        PackageExample p =new PackageExample();
+        p.m1("Hello world new");
+    }
+}
